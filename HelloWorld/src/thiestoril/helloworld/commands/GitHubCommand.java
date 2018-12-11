fbcada917a383964be3b742ca9f35734517a5c05
ThiEstoRil <45766139+ThiEstoRil@users.noreply.github.com>
2018-12-11 21:04:47 +0100

    GitHub link command
    Added the first command that sends to the player the link of my GitHub account when he type /helloworld:github in the chat

---
+++ HelloWorld/src/thiestoril/helloworld/commands/GitHubCommand.java
@@ -0,0 +1,30 @@
+package thiestoril.helloworld.commands;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import thiestoril.helloworld.HelloWorld;
+
+public class GitHubCommand implements CommandExecutor{
+	
+	private HelloWorld plugin;
+	
+	public GitHubCommand(HelloWorld plugin) {
+		this.plugin = plugin;
+	}
+	
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		if(!(sender instanceof Player)) {
+			Bukkit.getConsoleSender().sendMessage(ChatColor.RED+"You cannot execute this command in the console");
+			return false;
+		} else {
+			Player jugador = (Player) sender;
+			jugador.sendMessage("My GitHub account: "+ChatColor.AQUA+"https://github.com/ThiEstoRil");
+			return true;
+		}
+	}
+}
